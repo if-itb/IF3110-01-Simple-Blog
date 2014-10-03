fc59b79b080da2d16be263e899429c2ac4e79a9b
runbow <dragoon.redeyed@gmail.com>
2014-10-03 09:56:59 +0700

    aside edited

--- new_post.php
+++ new_post.php
@@ -77,16 +77,10 @@
     <!-- <div class="footer-nav"><p></p></div> -->
     <div class="psi">&Psi;</div>
     <aside class="offsite-links">
-        Asisten IF3110 /
+        author /
         <a class="rss-link" href="#rss">RSS</a> /
         <br>
-        <a class="twitter-link" href="http://twitter.com/YoGiiSinaga">Yogi</a> /
-        <a class="twitter-link" href="http://twitter.com/sonnylazuardi">Sonny</a> /
-        <a class="twitter-link" href="http://twitter.com/fathanpranaya">Fathan</a> /
-        <br>
-        <a class="twitter-link" href="#">Renusa</a> /
-        <a class="twitter-link" href="#">Kelvin</a> /
-        <a class="twitter-link" href="#">Yanuar</a> /
+        <a class="twitter-link" href="#">Rama</a> /
         
     </aside>
 </footer>
