855cb88b292aeafb41e594e29438fe859fcc5e49
runbow <dragoon.redeyed@gmail.com>
2014-10-03 09:48:41 +0700

    first edit

--- index.php
+++ index.php
@@ -38,9 +38,9 @@
 <div class="wrapper">
 
 <nav class="nav">
-    <a style="border:none;" id="logo" href="index.html"><h1>Simple<span>-</span>Blog</h1></a>
+    <a style="border:none;" id="logo" href="index.php"><h1>Simple<span>-</span>Blog</h1></a>
     <ul class="nav-primary">
-        <li><a href="new_post.html">+ Tambah Post</a></li>
+        <li><a href="new_post.php">+ Tambah Post</a></li>
     </ul>
 </nav>
 
@@ -50,7 +50,7 @@
           <ul class="art-list-body">
             <li class="art-list-item">
                 <div class="art-list-item-title-and-time">
-                    <h2 class="art-list-title"><a href="post.html">Apa itu Simple Blog?</a></h2>
+                    <h2 class="art-list-title"><a href="post.php">Apa itu Simple Blog?</a></h2>
                     <div class="art-list-time">15 Juli 2014</div>
                     <div class="art-list-time"><span style="color:#F40034;">&#10029;</span> Featured</div>
                 </div>
@@ -62,7 +62,7 @@
 
             <li class="art-list-item">
                 <div class="art-list-item-title-and-time">
-                    <h2 class="art-list-title"><a href="post.html">Siapa dibalik Simple Blog?</a></h2>
+                    <h2 class="art-list-title"><a href="post.php">Siapa dibalik Simple Blog?</a></h2>
                     <div class="art-list-time">11 Juli 2014</div>
                 </div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis repudiandae quae natus quos alias eos repellendus a obcaecati cupiditate similique quibusdam, atque omnis illum, minus ex dolorem facilis tempora deserunt! &hellip;</p>
