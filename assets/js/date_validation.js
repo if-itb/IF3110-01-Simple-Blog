4957b804a5b1859d495377db44ad3e26a233dd8d
runbow <dragoon.redeyed@gmail.com>
2014-10-05 00:59:35 +0700

    re-fix bug validate date

--- assets/js/date_validation.js
+++ assets/js/date_validation.js
@@ -4,13 +4,18 @@ function validasitanggal(inputText)
 	{
 		document.getElementById("simpan").disabled = false;
 	}
+	else
+	{
+		document.getElementById("simpan").disabled = true;
+	} 
 }
 
 function validatedate(inputText)  
   {
   //var dateformat = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;  
   var dateformat = /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
-	var r=true;
+	var r;
+	r=true;
 	
   // Match the date format through regular expression  
   if(inputText.value.match(dateformat))  
@@ -40,25 +45,24 @@ function validatedate(inputText)
   {
 	alert('Tanggal salah!');  
 	r=false;
-  }else{
-	if (yy==d.getFullYear())
-	{
+  }
+   else{
+	
 		if ( mm<d.getMonth()+1)
 		{
 		alert('Tanggal salah!');  
 		r=false;
 		}else
 		{
-		if (mm==getMonth()+1)
-		{
+		
 			if (dd<d.getDate())
 			{
 			alert('Tanggal salah!');  
 			r=false;
 			}
+		
 		}
-	}
-  }
+	
   }
   if (mm==1 || mm>2)  
   {  
