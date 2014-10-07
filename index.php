204b4a4de0fdfe2bfbba5a0e68ee7191449c9003
Asep Saepudin <ibnu.komarudin@gmail.com>
2014-10-07 20:27:21 +0700

    clean up

--- index.php
+++ index.php
@@ -48,29 +48,10 @@
 	<div class="posts">
 		<nav class="art-list">
 		  <ul class="art-list-body">
-			<?php                               
-			  function printReadableDateFormat($tanggal) {
-				$rawDate = substr($tanggal, 0, 10);
-				$splitDate = explode("-", $rawDate);
-				switch ($splitDate[1]) {
-				  case 1: $month = 'Januari'; break;
-				  case 2: $month = 'Februari'; break;
-				  case 3: $month = 'Maret'; break;
-				  case 4: $month = 'April'; break;
-				  case 5: $month = 'Mei'; break;
-				  case 6: $month = 'Juni'; break;
-				  case 7: $month = 'Juli'; break;
-				  case 8: $month = 'Agustus'; break;
-				  case 9: $month = 'September'; break;
-				  case 10: $month = 'Oktober'; break;
-				  case 11: $month = 'November'; break;
-				  case 12: $month = 'Desember'; break;
-				  default: break;
-				}
-				echo $splitDate[2].' '.$month.' '.$splitDate[0];
-			  }
+			<?php                               			  
 
-			  include 'dbconnect.php';       
+			  include 'header.php';
+			  include 'dbconnect.php';
 
 			  $sqlQuery = "SELECT * FROM `simple_blog`.`post` ORDER BY `tanggal` DESC";
 			  $result = mysqli_query($connDb, $sqlQuery);              
@@ -95,7 +76,7 @@
 </div>
 
 <footer class="footer">
-	<div class="back-to-top"><a href="">Back to top</a></div>
+	<div class="back-to-top"><a href="#">Back to top</a></div>
 	<!-- <div class="footer-nav"><p></p></div> -->
 	<div class="psi">&Psi;</div>
 	<aside class="offsite-links">
