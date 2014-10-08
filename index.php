233c9692a35ae8cb57ac45260cbed10242a248b8
Ichlasul Amal <ichlasul.a@gmail.com>
2014-10-08 10:30:55 +0700

    Add post.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
             <ul class="art-list-body">
               <li class="art-list-item">
                   <div class="art-list-item-title-and-time">
-                      <h2 class="art-list-title"><a href="post.html">Apa itu Simple Blog?</a></h2>
+                      <h2 class="art-list-title"><a href="post.php">Apa itu Simple Blog?</a></h2>
                       <div class="art-list-time">15 Juli 2014</div>
                       <div class="art-list-time"><span style="color:#F40034;">&#10029;</span> Featured</div>
                   </div>
@@ -30,7 +30,7 @@
 
               <li class="art-list-item">
                   <div class="art-list-item-title-and-time">
-                      <h2 class="art-list-title"><a href="post.html">Siapa dibalik Simple Blog?</a></h2>
+                      <h2 class="art-list-title"><a href="post.php">Siapa dibalik Simple Blog?</a></h2>
                       <div class="art-list-time">11 Juli 2014</div>
                   </div>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis repudiandae quae natus quos alias eos repellendus a obcaecati cupiditate similique quibusdam, atque omnis illum, minus ex dolorem facilis tempora deserunt! &hellip;</p>
---
+++ post.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <?php $title = 'Simple Blog | Apa itu Simple Blog?'; ?>
+  <?php include 'templates/head.php'; ?>
+
+</head>
+
+<body class="default">
+  <div class="wrapper">
+
+    <?php include 'templates/header.php'; ?>
+
+    <article class="art simple post">
+        
+      <header class="art-header">
+        <div class="art-header-inner" style="margin-top: 0px; opacity: 1;">
+          <time class="art-time">15 Juli 2014</time>
+          <h2 class="art-title">Apa itu Simple Blog?</h2>
+          <p class="art-subtitle"></p>
+        </div>
+      </header>
+
+      <div class="art-body">
+        <div class="art-body-inner">
+          <hr class="featured-article" />
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis aliquam minus consequuntur amet nulla eius, neque beatae, nostrum possimus, officiis eaque consectetur. Sequi sunt maiores dolore, illum quidem eos explicabo! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam consequuntur consequatur molestiae saepe sed, incidunt sunt inventore minima voluptatum adipisci hic, est ipsa iste. Nobis, aperiam   provident quae. Reprehenderit, iste.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores animi tenetur nam delectus eveniet iste non culpa laborum provident minima numquam excepturi rem commodi, officia accusamus eos voluptates obcaecati. Possimus?</p>
+
+          <hr />
+            
+          <h2>Komentar</h2>
+
+          <div id="contact-area">
+            <form method="post" action="#">
+              <label for="Nama">Nama:</label>
+              <input type="text" name="Nama" id="Nama">
+  
+              <label for="Email">Email:</label>
+              <input type="text" name="Email" id="Email">
+              
+              <label for="Komentar">Komentar:</label><br>
+              <textarea name="Komentar" rows="20" cols="20" id="Komentar"></textarea>
+
+              <input type="submit" name="submit" value="Kirim" class="submit-button">
+            </form>
+          </div>
+
+          <ul class="art-list-body">
+            <li class="art-list-item">
+              <div class="art-list-item-title-and-time">
+                  <h2 class="art-list-title"><a href="post.html">Jems</a></h2>
+                  <div class="art-list-time">2 menit lalu</div>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis repudiandae quae natus quos alias eos repellendus a obcaecati cupiditate similique quibusdam, atque omnis illum, minus ex dolorem facilis tempora deserunt! &hellip;</p>
+            </li>
+
+            <li class="art-list-item">
+              <div class="art-list-item-title-and-time">
+                  <h2 class="art-list-title"><a href="post.html">Kave</a></h2>
+                  <div class="art-list-time">1 jam lalu</div>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis repudiandae quae natus quos alias eos repellendus a obcaecati cupiditate similique quibusdam, atque omnis illum, minus ex dolorem facilis tempora deserunt! &hellip;</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </article>
+
+    <?php include 'templates/footer.php'; ?>
+
+  </div>
+
+</body>
+</html>
