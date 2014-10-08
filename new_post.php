235f1223b1ff478bea8fd8d3f930df4fd00ba772
Ichlasul Amal <ichlasul.a@gmail.com>
2014-10-08 10:38:58 +0700

    Add new_post.php

---
+++ new_post.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <?php $title = 'Simple Blog | Tambah Post'; ?>
+  <?php include 'templates/head.php'; ?>
+
+</head>
+
+<body class="default">
+<div class="wrapper">
+
+  <?php include 'templates/header.php'; ?>
+
+  <article class="art simple post">
+          
+    <h2 class="art-title" style="margin-bottom:40px">-</h2>
+
+    <div class="art-body">
+      <div class="art-body-inner">
+        <h2>Tambah Post</h2>
+
+        <div id="contact-area">
+          <form method="post" action="#">
+            <label for="Judul">Judul:</label>
+            <input type="text" name="Judul" id="Judul">
+
+            <label for="Tanggal">Tanggal:</label>
+            <input type="text" name="Tanggal" id="Tanggal">
+            
+            <label for="Konten">Konten:</label><br>
+            <textarea name="Konten" rows="20" cols="20" id="Konten"></textarea>
+
+            <input type="submit" name="submit" value="Simpan" class="submit-button">
+          </form>
+        </div>
+      </div>
+    </div>
+
+  </article>
+
+  <?php include 'templates/footer.php'; ?>
+
+</div>
+
+<?php include 'templates/foot.php'; ?>
+
+</body>
+</html>
