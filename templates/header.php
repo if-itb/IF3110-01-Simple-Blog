b6176162c1a58086feb9014e29d694fb6a112950
Ichlasul Amal <ichlasul.a@gmail.com>
2014-10-08 19:48:26 +0700

    Change link in header

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
 <nav class="nav">
-  <a id="logo" href="index.html"><h1>Simple<span>-</span>Blog</h1></a>
+  <a id="logo" href="<?php echo $CONFIG['siteurl'].'/index.php'; ?>"><h1>Simple<span>-</span>Blog</h1></a>
   <ul class="nav-primary">
     <li><a href="new_post.php">+ Tambah Post</a></li>
   </ul>
