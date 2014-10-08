54c7baaa7b26a7e5f8d83f89b37679d1356e2ba9
Jan Wira Gotama Putra <wiragotama@gmail.com>
2014-10-08 20:02:03 +0700

    Delete validateDelete.js

--- Tubes 1 WBD/JS/validateDelete.js
+++
@@ -1,8 +0,0 @@
-function validateDeletion() {
-/*	Post deletion confirmation */
-	var answer = confirm ("Are you sure want to delete this post?");
-	if (answer) {
-		return true;
-	}
-	else return false;
-}
