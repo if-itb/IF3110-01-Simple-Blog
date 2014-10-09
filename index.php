3a12da3433ad056aee16ff8e61304ac59404b3e4
Ichlasul Amal <ichlasul.a@gmail.com>
2014-10-10 00:01:38 +0700

    remove controllers

--- index.php
+++ index.php
@@ -5,4 +5,18 @@
   require_once 'helpers/datetime.php';
   require_once 'helpers/url.php';
 
-  require 'system/routes.php'; 
+  $route = empty($_GET['route']) ? 'post' : $_GET['route'];
+  
+  $parts = explode('/', $route);
+  $CONTROLLER = $parts[0];
+  if (isset($parts[1])) {
+    $ACTION = $parts[1];
+  } else {
+    $ACTION = 'index';
+  }
+
+  if (isset($parts[2])) {
+    $PARAM = $parts[2];
+  }
+  
+  require 'controllers/'.$CONTROLLER.'.php';
