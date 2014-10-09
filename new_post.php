fb5e879190868f1fb5c0cafb0cfd21220450330d
Ichlasul Amal <ichlasul.a@gmail.com>
2014-10-09 20:26:29 +0700

    Add new_post views

--- new_post.php
+++ new_post.php
@@ -21,59 +21,9 @@
   if (isset($_GET['id'])) {   
     $result = readPost((int) $_GET['id']);
     if ($result->num_rows > 0) {
-      $row = mysqli_fetch_array($result); 
+      $data = mysqli_fetch_array($result); 
     }
   }
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-
-  <?php $title = 'Simple Blog | Tambah Post'; ?>
-  <?php include 'views/templates/head.php'; ?>
-
-</head>
-
-<body class="default">
-<div class="wrapper">
-
-  <?php include 'views/templates/header.php'; ?>
-
-  <article class="art simple post">
-          
-    <h2 class="art-title"></h2>
-
-    <div class="art-body">
-      <div class="art-body-inner">
-        <h2>Tambah Post</h2>
-
-        <div id="contact-area">
-          <form method="post" action="" onsubmit="return validatePost(this)">
-            <input type="hidden" name="id" id="id" value="<?php echo isset($row) ? $row['post_id'] : '' ?>" id="EditMode">
-
-            <label for="Judul">Judul:</label>
-            <input type="text" name="Judul" id="Judul" value="<?php echo isset($row) ? $row['post_title'] : ''; ?>">
-
-            <label for="Tanggal">Tanggal:</label>
-            <input type="text" name="Tanggal" id="Tanggal" placeholder="dd-mm-yyyy" value="<?php echo isset($row) ? $row['post_date'] : ''; ?>">
-            
-            <label for="Konten">Konten:</label><br>
-            <textarea name="Konten" rows="20" cols="20" id="Konten"><?php echo isset($row) ? $row['post_content'] : ''; ?></textarea>
-
-            <input type="submit" name="submit" value="Simpan" class="submit-button">
-          </form>
-        </div>
-      </div>
-    </div>
-
-  </article>
-
-  <?php include 'views/templates/footer.php'; ?>
-
-</div>
-
-<?php include 'views/templates/foot.php'; ?>
-
-</body>
-</html>
+  include 'views/new_post.php';
+  die();
