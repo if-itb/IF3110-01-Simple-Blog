182f80ddc4854538e9abee6463c45beb59fa3dba
Ichlasul Amal <ichlasul.a@gmail.com>
2014-10-09 18:32:31 +0700

    'x menit yg lalu' success

--- system/datetime.php
+++ system/datetime.php
@@ -8,8 +8,22 @@
 	}
 
   function datetimeBeautifier($str) {
-    setlocale(LC_TIME, "");
-    setlocale(LC_TIME, "id_ID");
-    //Contoh: 1 Januari 2015
-    return strftime("%#d %B %Y Pukul %H:%M", strtotime($str));
+    $datetime =  strtotime($str);
+    $interval =  date_create($str)->diff(date_create('now'));
+
+    if ($interval->days > 0) {
+      setlocale(LC_TIME, "");
+      setlocale(LC_TIME, "id_ID");
+      return strftime("%#d %B %Y </br> Jam %H:%M", strtotime($str));
+    }
+
+    if ($interval->h >= 1) {
+      return $interval->h." jam yang lalu";    
+    }
+
+    if ($interval->i >= 1) {
+      return $interval->i." menit yang lalu";   
+    } 
+
+    return "Beberapa saat yang lalu";
   }
