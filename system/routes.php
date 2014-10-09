cc602ea140ed0e3e2d3fa85104e80f5ffbdeb081
Ichlasul Amal <ichlasul.a@gmail.com>
2014-10-09 22:37:21 +0700

    Add routes

---
+++ system/routes.php
@@ -0,0 +1,15 @@
+<?php
+  
+  $route = empty($_GET['route'])) ? 'post' : $_GET['route'];
+  
+  $parts = explode('/', $route);
+  $CONTROLLER = $parts[0];
+  if (isset($parts[1])) {
+    $ACTION = $parts[1];
+  }
+
+  if (isset($parts[2])) {
+    $PARAM = $parts[2];
+  }
+	
+  require 'controllers/'.$CONTROLLER.'.php';
