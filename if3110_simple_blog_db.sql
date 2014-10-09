856211f42b71c608171adc4a4eeac2583c9135c3
jeffhorus <jeffhorus19@gmail.com>
2014-10-09 11:25:15 +0700

    database versi terbaru

--- if3110_simple_blog_db.sql
+++ if3110_simple_blog_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2014 at 01:37 PM
+-- Generation Time: Oct 09, 2014 at 06:24 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -35,15 +35,16 @@ CREATE TABLE IF NOT EXISTS `sb_comments` (
   `id_post` int(4) NOT NULL,
   PRIMARY KEY (`id_komentar`),
   KEY `id_post` (`id_post`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `sb_comments`
 --
 
 INSERT INTO `sb_comments` (`id_komentar`, `nama`, `email`, `komentar`, `timestamp`, `id_post`) VALUES
-(31, 'Jeffrey Lingga', 'jeffhorus19@gmail.com', 'masih bakal error gak ya?', '2014-10-04 10:25:06', 26),
-(32, 'Jeffrey Lingga Binangkit', 'jeffhorus19@gmail.com', 'masih bakal error gak ya?', '2014-10-04 10:25:44', 26);
+(5, 'Ardi', 'ardi@ardi.com', 'kocak bro\nhaha', '2014-10-09 04:11:15', 42),
+(6, 'Riky', 'riky@riky.com', 'wah bisa lebih dari 30 kata kepotong', '2014-10-09 04:11:47', 27),
+(7, 'Bagaskara', 'bagas@bagas.com', 'Gak error kok bro', '2014-10-09 04:12:41', 26);
 
 -- --------------------------------------------------------
 
@@ -57,15 +58,16 @@ CREATE TABLE IF NOT EXISTS `sb_posts` (
   `tanggal` date NOT NULL COMMENT 'tanggal publikasi post',
   `konten` text NOT NULL COMMENT 'isi post',
   PRIMARY KEY (`id_post`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=50 ;
 
 --
 -- Dumping data for table `sb_posts`
 --
 
 INSERT INTO `sb_posts` (`id_post`, `judul`, `tanggal`, `konten`) VALUES
-(26, 'Ini Post Pertama ke Sekian', '2014-10-04', 'jangan error ya'),
-(27, 'Pos Kedua', '2014-10-04', 'Ini pos kedua dengan panjang file melebihi 30 kata.\r\nIni pos kedua dengan panjang file melebihi 30 kata.\r\nIni pos kedua dengan panjang file melebihi 30 kata.\r\nIni pos kedua dengan panjang file melebihi 30 kata.\r\nIni pos kedua dengan panjang file melebihi 30 kata.');
+(26, 'Ini Post Pertama ke Sekian', '2014-10-05', 'jangan error ya'),
+(27, 'Pos Kedua', '2014-10-05', 'Ini pos kedua dengan panjang file melebihi 30 kata.\r\nIni pos kedua dengan panjang file melebihi 30 kata.\r\nIni pos kedua dengan panjang file melebihi 30 kata.\r\nIni pos kedua dengan panjang file melebihi 30 kata.\r\nIni pos kedua dengan panjang file melebihi 30 kata.'),
+(42, 'Main Kata', '2014-10-05', 'Mari bermain kata, tapi jangan kebanyakan, soalnya kalau kebanyakan nanti ilang. ilang?? ilang gimana?? nih ku kasih contoh. Kalau kata dalam pos lebih dari 30 bisa gak muncul lho isinya. Kok bisa?? kalo ga percaya coba nih gw ulang lagi isi paragrafnya. Mari bermain kata, tapi jangan kebanyakan, soalnya kalau kebanyakan nanti ilang. ilang?? ilang gimana?? nih ku kasih contoh. Kalau kata dalam pos lebih dari 30 bisa gak muncul lho isinya. Kok bisa?? kalo ga percaya coba nih gw ulang lagi isi paragrafnya. ');
 
 --
 -- Constraints for dumped tables
