458932bcd350effe66261014da38f108c122ec50
Rikysamuel Taniman <rikz.samuel@gmail.com>
2014-10-10 15:32:08 +0700

    validasi tanggal

--- assets/js/confirm.js
+++ assets/js/confirm.js
@@ -14,7 +14,7 @@
 
   function checkemail()
   {
-    var email=document.getElementById('email')
+    var email=document.getElementById('email');
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
   }
@@ -28,6 +28,27 @@
 
   function checkformat(tanggal)
   {
-  	
+    //alert(tanggal);
+  	var re = /^[0-9]{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/;
+
+    var today = new Date();
+    var date = tanggal.match(re);
+    //alert("date : " + date);
+    var dtYear = date[0][0]+date[0][1]+date[0][2]+date[0][3];
+    var dtMonth = date[0][5]+date[0][6];
+    var dtDay=  date[0][8]+date[0][9];
+
+    var tYear = today.getFullYear().toString();
+    var tMonth = ("0" + (today.getMonth()+1)).slice(-2).toString();
+    var tDay = ("0" + today.getDate()).slice(-2).toString();
+
+    //waktu lebih kecil dari hari ini
+    if (dtYear<tYear){
+      if (dtMonth<tMonth){
+        if (dtDay<tDay){
+          document.getElementById('date_comment').innerHTML="Tanggal tidak boleh lebih kecil dari hari ini!";
+        }
+      }
+    }
   }
 
