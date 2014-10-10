21ad54dd5a9c073ce6ceac28d3812512f0430afb
Anandakurniawan <kurniawan.ananda@gmail.com>
2014-10-10 14:41:34 +0700

    Tambah post

---
+++ new_post.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="description" content="Deskripsi Blog">
+<meta name="author" content="Judul Blog">
+
+<!-- Twitter Card -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="omfgitsasalmon">
+<meta name="twitter:title" content="Simple Blog">
+<meta name="twitter:description" content="Deskripsi Blog">
+<meta name="twitter:creator" content="Simple Blog">
+<meta name="twitter:image:src" content="{{! TODO: ADD GRAVATAR URL HERE }}">
+
+<meta property="og:type" content="article">
+<meta property="og:title" content="Simple Blog">
+<meta property="og:description" content="Deskripsi Blog">
+<meta property="og:image" content="{{! TODO: ADD GRAVATAR URL HERE }}">
+<meta property="og:site_name" content="Simple Blog">
+
+<link rel="stylesheet" type="text/css" href="assets/css/screen.css" />
+<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+
+<!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<title>Simple Blog | Tambah Post</title>
+
+<?php
+
+require_once('config.php');
+//if form has been submitted process it
+if(isset($_POST['submit'])){
+
+	$_POST = array_map( 'stripslashes', $_POST );
+
+	//collect form data
+	extract($_POST);
+
+	//very basic validation
+	if($postTitle ==''){
+		$error[] = 'Please enter the title.';
+	}
+
+	if($postDesc ==''){
+		$error[] = 'Please enter the description.';
+	}
+
+	if($postCont ==''){
+		$error[] = 'Please enter the content.';
+	}
+
+	if(!isset($error)){
+
+		try {
+
+			//insert into database
+			$stmt = $db->prepare('INSERT INTO blog_posts (postTitle,postDesc,postCont,postDate) VALUES (:postTitle, :postDesc, :postCont, :postDate)') ;
+			$stmt->execute(array(
+				':postTitle' => $postTitle,
+				':postDesc' => $postDesc,
+				':postCont' => $postCont,
+				':postDate' => date('Y-m-d H:i:s')
+			));
+
+			//redirect to index page
+			header('Location: index.php?action=added');
+			exit;
+
+		} catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+
+	}
+
+}
+
+//check for any errors
+if(isset($error)){
+	foreach($error as $error){
+		echo '<p class="error">'.$error.'</p>';
+	}
+}
+?>
+
+</head>
+
+<body class="default">
+<div class="wrapper">
+
+<nav class="nav">
+    <a style="border:none;" id="logo" href="index.html"><h1>Simple<span>-</span>Blog</h1></a>
+    <ul class="nav-primary">
+        <li><a href="new_post.html">+ Tambah Post</a></li>
+    </ul>
+</nav>
+
+<article class="art simple post">
+    
+    
+    <h2 class="art-title" style="margin-bottom:40px">-</h2>
+
+    <div class="art-body">
+        <div class="art-body-inner">
+            <h2>Tambah Post</h2>
+
+            <div id="contact-area">
+                <form method="post" action=''>
+                    <label for="Judul">Judul:</label>
+                    <input type="text" name="postTitle" id="Judul" value='
+					<?php if(isset($error)){ echo $_POST['postTitle'];}?>'>
+
+                    <label for="Tanggal">Tanggal:</label>
+                    <input type="text" name="Tanggal" id="Tanggal">
+                    
+					<label for="Konten">Konten:</label><br>
+                    <textarea name="postCont" rows="20" cols="20" id="Konten">
+					<?php if(isset($error)){ echo $_POST['postCont'];}?>
+					</textarea>
+					
+                    <label for="Deskripsi">Deskripsi:</label><br>
+                    <textarea name="postDesc" rows="20" cols="20" id="Konten">
+					<?php if(isset($error)){ echo $_POST['postDesc'];}?>
+					</textarea>
+
+                    <input type="submit" name="submit" value="Submit" class="submit-button">
+                </form>
+            </div>
+        </div>
+    </div>
+
+</article>
+
+<footer class="footer">
+    <div class="back-to-top"><a href="">Back to top</a></div>
+    <!-- <div class="footer-nav"><p></p></div> -->
+    <div class="psi">&Psi;</div>
+    <aside class="offsite-links">
+        Asisten IF3110 /
+        <a class="rss-link" href="#rss">RSS</a> /
+        <br>
+        <a class="twitter-link" href="http://twitter.com/YoGiiSinaga">Yogi</a> /
+        <a class="twitter-link" href="http://twitter.com/sonnylazuardi">Sonny</a> /
+        <a class="twitter-link" href="http://twitter.com/fathanpranaya">Fathan</a> /
+        <br>
+        <a class="twitter-link" href="#">Renusa</a> /
+        <a class="twitter-link" href="#">Kelvin</a> /
+        <a class="twitter-link" href="#">Yanuar</a> /
+        
+    </aside>
+</footer>
+
+</div>
+
+<script type="text/javascript" src="assets/js/fittext.js"></script>
+<script type="text/javascript" src="assets/js/app.js"></script>
+<script type="text/javascript" src="assets/js/respond.min.js"></script>
+<script type="text/javascript">
+  var ga_ua = '{{! TODO: ADD GOOGLE ANALYTICS UA HERE }}';
+
+  (function(g,h,o,s,t,z){g.GoogleAnalyticsObject=s;g[s]||(g[s]=
+      function(){(g[s].q=g[s].q||[]).push(arguments)});g[s].s=+new Date;
+      t=h.createElement(o);z=h.getElementsByTagName(o)[0];
+      t.src='//www.google-analytics.com/analytics.js';
+      z.parentNode.insertBefore(t,z)}(window,document,'script','ga'));
+      ga('create',ga_ua);ga('send','pageview');
+</script>
+
+</body>
+</html>
