96974714ab887960a6f9667d7067bcf7ba764b72
hayyuelha <heripottypot@gmail.com>
2014-10-13 00:02:47 +0700

    JavaScript untuk validasi tanggal pada form Post dan Edit, belum berjalan dengan benar

---
+++ assets/js/validate_date.js
@@ -0,0 +1,8 @@
+function validate_date(){
+	var x=document.forms["newPost"]["Tanggal"].value;
+	var inputDateString = new String();
+	
+	var now = new Date();
+	var inputDate = new Date();
+	inputDate.setFullYear()
+}
