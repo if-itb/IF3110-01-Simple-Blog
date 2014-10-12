4eddef567af875755ba65dec0ba2c6a72fb13f3a
wiragotama <wiragotama@gmail.com>
2014-10-12 13:55:08 +0700

    perbaikan

--- Tubes 1 WBD/PHP/savePost.php
+++ Tubes 1 WBD/PHP/savePost.php
@@ -16,14 +16,13 @@
 	else {		
 		$result = mysqli_query($con, "SELECT * FROM post where title=".$x);
 		if (!$result) { //post with the same title not exist
+			$time = date("H:i:s"); 
 			if ($f!=0) { //update db
-				mysqli_query($con, "UPDATE post SET title='".$x."', content='".$z."' WHERE id=".$f);
+				mysqli_query($con, "UPDATE post SET title='".$x."', datePost='".$y."', timePost='".$time."', content='".$z."' WHERE id=".$f);
 			}
 			else { //insert to db
 				$var = mysqli_query($con, "SELECT MAX(id) as 'id' FROM post");
 				$last = mysqli_fetch_array($var, MYSQL_ASSOC);
-				
-				$time = date("H:i:s"); 
 				$result = mysqli_query($con, "INSERT INTO post VALUES ( ".($last["id"]+1).", '".$x."', '".$y."', '".$time."', '".$z."')");
 			}
 			$url = '../VIEW/home.php';
