55ca440758e7ef5863d66b497bfbcbe756288a59
ClearingPath <13512093@std.stei.itb.ac.id>
2014-10-12 09:55:51 +0700

    Validation of newpost is dont, submited to mysql

--- assets/js/postvalidation.js
+++ assets/js/postvalidation.js
@@ -1,34 +1,93 @@
 function checkDate(field) { 
 	var allowBlank = true; 
 	var minDate = (new Date()).getDate();
-	var minMonth = (new Date()).getMonth();
+	var minMonth = (new Date()).getMonth() + 1;
 	var minYear = (new Date()).getFullYear(); 
-	var today = new Date();
+	var today = minDate +"/" + minMonth + "/" + minYear;
 	var errorMsg = ""; 
 
 	// regular expression to match required date format
  	re = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/; 
  	if(field.value != '') { 
  		if(regs = field.value.match(re)) { 
- 			if(regs[1] < 1 || regs[1] > 31 || regs[1] < minDate) { 
- 				errorMsg = "Invalid value for day: " + regs[1]; 
- 				if (regs[1] < minDate)
- 				{
- 					errorMsg = errorMsg+". Value is smaller than today" + today;
+ 			var isNotKabisat = (regs[3] % 400 != 0) || (regs[3] % 100 == 0 && regs[3] % 400 != 0) || (regs[3] % 4 != 0);
+ 			if (regs[3] < minYear){
+ 				errorMsg = errorMsg + "- Year is smaller than today(" + today +").\n";
+ 			}
+ 			else //the year is true
+ 			{
+ 				if (regs[2] < 1 || regs[2] > 12){
+ 					errorMsg = errorMsg + "- Month is not valid " + regs[2];
  				}
- 			} 
- 			else if(regs[2] < 1 || regs[2] > 12 ) { 
- 				errorMsg = "Invalid value for month: " + regs[2]; 
- 			} 
- 			else if(regs[3] < minYear || regs[3] > maxYear) { 
- 				errorMsg = "Invalid value for year: " + regs[3] + " - must be between " + minYear + " and " + maxYear; } 
- 			} 
- 		else { errorMsg = "Invalid date format: " + field.value; } 
+ 				else
+ 				{
+ 					if (regs[2] < minMonth && regs[3] == minYear){
+ 						errorMsg = errorMsg + "- Month is smaller than today(" + today +").\n";
+ 					}
+ 					else
+ 					{
+ 						//31-days month
+ 						if (regs[2] == 1 || regs[2] == 3 || regs[2] == 5 || regs[2] == 7 || regs[2] == 8 || regs[2] == 10 || regs[2] == 12 ){
+ 							if (regs[1] < 1 || regs[1] > 31){
+ 								errorMsg = errorMsg +"- Date is not valid " + regs[1];
+ 							}
+ 							else
+ 							{
+ 								if (regs[1] < minDate && regs[2] == minMonth){
+ 									errorMsg = errorMsg + "- Date is smaller than today(" + today +").\n";
+ 								}
+ 							}
+ 						}
+ 						if (regs[2] == 2)
+ 						{
+ 							if (isNotKabisat)
+ 							{
+ 								if (regs[1] < 1 || regs[1] > 28){
+	 								errorMsg = errorMsg +"- Date is not valid " + regs[1];
+	 							}
+	 							else
+	 							{
+	 								if (regs[1] < minDate && regs[2] == minMonth){
+	 									errorMsg = errorMsg + "- Date is smaller than today(" + today +").\n";
+	 								}
+	 							}
+ 							}
+ 							else
+ 							{
+ 								if (regs[1] < 1 || regs[1] > 28){
+	 								errorMsg = errorMsg +"- Date is not valid " + regs[1];
+	 							}
+	 							else
+	 							{
+	 								if (regs[1] < minDate && regs[2] == minMonth){
+	 									errorMsg = errorMsg + "- Date is smaller than today(" + today +").\n";
+	 								}
+	 							}
+ 							}
+ 						}
+ 						else
+ 						{
+ 							if (regs[1] < 1 || regs[1] > 30){
+ 								errorMsg = errorMsg +"- Date is not valid " + regs[1];
+ 							}
+ 							else
+ 							{
+ 								if (regs[1] < minDate && regs[2] == minMonth){
+ 									errorMsg = errorMsg + "- Date is smaller than today(" + today +").\n";
+ 								}
+ 							}
+ 						}
+ 					}
+ 				}
+ 				
+ 			}
+ 		} 
+ 		else { errorMsg = "- Invalid date format: " + field.value; } 
  	} 
  	else if(!allowBlank) { errorMsg = "Empty date not allowed!"; 
  	} 
  	if(errorMsg != "") { 
- 		alert(errorMsg); 
+ 		alert("Date is not valid for some reason :\n" +errorMsg); 
  		field.focus(); 
  		return false; 
  			} 
