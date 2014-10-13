8a5a8d790516a7cefc1096cce8867785c9c764f0
calvinsadewa <master23680@gmail.com>
2014-10-13 21:07:13 +0700

    debugging fitur penambahan post

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -12,9 +12,9 @@ var returnval=false
 if (!validformat.test(input))
 alert("Invalid Date Format. Please correct and submit again.")
 else{ //Detailed check for valid date ranges
-var monthfield=input.value.split("/")[1]
-var dayfield=input.value.split("/")[0]
-var yearfield=input.value.split("/")[2]
+var monthfield=input.split("/")[1]
+var dayfield=input.split("/")[0]
+var yearfield=input.split("/")[2]
 var dayobj = new Date(yearfield, monthfield-1, dayfield)
 if ((dayobj.getMonth()+1!=monthfield)||(dayobj.getDate()!=dayfield)||(dayobj.getFullYear()!=yearfield))
 alert("Invalid Day, Month, or Year range detected. Please correct and submit again.")
