03617adf8afbd801ab43aa61744e01c0e4400ed0
hayyuelha <heripottypot@gmail.com>
2014-10-13 23:35:44 +0700

    belum berjalan dengan benar

--- assets/js/konfirmasi.js
+++
@@ -1,8 +0,0 @@
-function konfirmasi(){
-	var conf=document.forms["deleteconfirm"]["submit"].value;
-	if (conf=="Ya"){
-		return true;
-	} else if (conf=="Tidak") {
-		return false;
-	}
-}
--- assets/js/validate_date.js
+++ assets/js/validate_date.js
@@ -1,8 +1,23 @@
-function validate_date(){
-	var x=document.forms["newPost"]["Tanggal"].value;
-	var inputDateString = new String();
-	
-	var now = new Date();
-	var inputDate = new Date();
-	inputDate.setFullYear()
+function validate_date(input){
+	var validformat=/^\d{2}\/\d{2}\/\d{4}$/ ;//Basic check for format validity
+	var returnval=false;
+	if (!validformat.test(input.value)) {
+		alert("Invalid Date Format. Please correct and submit again.");
+	}
+	else{ //Detailed check for valid date ranges
+		var dayfield=input.value.split("-")[0];
+		var monthfield=input.value.split("-")[1];
+		var yearfield=input.value.split("-")[2];
+		var dayobj = new Date(yearfield, monthfield-1, dayfield);
+		if ((dayobj.getMonth()+1!=monthfield)||(dayobj.getDate()!=dayfield)||(dayobj.getFullYear()!=yearfield)){
+			alert("Invalid Day, Month, or Year range detected. Please correct and submit again.");
+		} else {
+		returnval=true;
+		}
+	}
+	if (returnval==false){
+		input.select();
+	}
+	return returnval;
 }
+
