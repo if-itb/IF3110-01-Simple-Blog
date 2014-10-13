9422e8a1a4091d8e1541aab138097cdaa1a8ad96
adwisatya <a.dwisaty4@yahoo.com>
2014-10-13 17:07:09 +0700

    Hampir beres

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 <div class="wrapper">
 
 <nav class="nav">
-    <a style="border:none;" id="logo" href="index.php"><h1>Simple<span>-</span>Blog</h1></a>
+    <a style="border:none;" id="logo" href="index.php"><h1>Simple-Blog<span>-of-</span>Bangsatya</h1></a>
     <ul class="nav-primary">
         <li><a href="new_post.html">+ Tambah Post</a></li>
     </ul>
