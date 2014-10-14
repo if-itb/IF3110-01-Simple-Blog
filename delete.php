8f8604e69fc2c9456a5eb96d2757fe0b2b157dde
hayyuelha <heripottypot@gmail.com>
2014-10-15 02:51:35 +0700

    penambahan query delete comment

--- delete.php
+++ delete.php
@@ -13,6 +13,10 @@ if (!empty($_GET['pid'])){
 		die('Tidak bisa tersambung: ' . mysql_error());
 	}
 	mysql_select_db("simpleblog");
+	$deleteComment = "DELETE FROM komentar WHERE pid='$pid'";
+	if (!mysql_query($deleteComment,$sambung)){
+		die('Error: ' . mysql_error());
+	}
 	$deletePost = "DELETE FROM blogitem WHERE pid='$pid' ";
 	if (!mysql_query($deletePost,$sambung)){
 		die('Error: ' . mysql_error());
