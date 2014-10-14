9e8fa1526472816f0624905c496e1a19ed0a0d3c
hayyuelha <heripottypot@gmail.com>
2014-10-14 18:59:32 +0700

    validasi tanggal sudah berjalan dengan benar

--- assets/js/validate_date.js
+++ assets/js/validate_date.js
@@ -1,23 +1,41 @@
-function validate_date(input){
-	var validformat=/^\d{2}\/\d{2}\/\d{4}$/ ;//Basic check for format validity
-	var returnval=false;
-	if (!validformat.test(input.value)) {
-		alert("Invalid Date Format. Please correct and submit again.");
+function validate_date()
+{
+	var tanggal = document.getElementById('Tanggal').value;
+	document.getElementById('errormsg').innerHTML="";
+	var tanggal1 = strToDate(tanggal);
+	if(tanggal1 == null)
+	{
+		return false;
 	}
-	else{ //Detailed check for valid date ranges
-		var dayfield=input.value.split("-")[0];
-		var monthfield=input.value.split("-")[1];
-		var yearfield=input.value.split("-")[2];
-		var dayobj = new Date(yearfield, monthfield-1, dayfield);
-		if ((dayobj.getMonth()+1!=monthfield)||(dayobj.getDate()!=dayfield)||(dayobj.getFullYear()!=yearfield)){
-			alert("Invalid Day, Month, or Year range detected. Please correct and submit again.");
-		} else {
-		returnval=true;
+	else
+	{
+		var tanggal2 = new Date();
+		var result = tanggal1 > tanggal2;
+		if(result)
+		{
+			var tanggal_reverse = tanggal.split("-").reverse().join("-");
+			document.getElementById('Tanggal').value=tanggal_reverse;
+			return true;
+		}
+		else
+		{
+			document.getElementById('errormsg').innerHTML="*Tanggal sudah lewat. Masukkan tanggal yang valid.";
+			return false;
 		}
 	}
-	if (returnval==false){
-		input.select();
-	}
-	return returnval;
 }
 
+function strToDate(str) {
+  var m = str.match(/^(\d{1,2})-(\d{1,2})-(\d{4})$/);
+  var tanggal3;
+  if(m)
+  {
+  	var tanggal3 = new Date(m[3],m[2]-1,m[1]-(-1));
+  }
+  else 	
+  {
+  	document.getElementById("errormsg").innerHTML="Format tanggal salah. (format:dd-mm-yyyy)";
+  	var tanggal3 = null;
+  }
+  return tanggal3;
+}
