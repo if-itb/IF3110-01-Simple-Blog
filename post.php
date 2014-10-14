d3d2aa0da1defa85a3315f91e588f1fbaed9c80c
lutfifadlan <13512087@std.stei.itb.ac.id>
2014-10-15 05:00:43 +0700

    Create post.php

---
+++ post.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+<?php
+	mysql_connect("localhost", "root", "");
+	mysql_select_db("simpleblog");
+	$var = $_GET['postid'];
+	$sql = mysql_query("SELECT * FROM post WHERE id = $var");
+	$row = mysql_fetch_array($sql);
+?>
+
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="description" content="Deskripsi Blog">
+<meta name="author" content="Judul Blog">
+
+<!-- Twitter Card -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="omfgitsasalmon">
+<meta name="twitter:title" content="Simple Blog">
+<meta name="twitter:description" content="Deskripsi Blog">
+<meta name="twitter:creator" content="Simple Blog">
+<meta name="twitter:image:src" content="{{! TODO: ADD GRAVATAR URL HERE }}">
+
+<meta property="og:type" content="article">
+<meta property="og:title" content="Simple Blog">
+<meta property="og:description" content="Deskripsi Blog">
+<meta property="og:image" content="{{! TODO: ADD GRAVATAR URL HERE }}">
+<meta property="og:site_name" content="Simple Blog">
+
+<link rel="stylesheet" type="text/css" href="assets/css/screen.css" />
+<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+
+<!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<title>Simple Blog | Apa itu Simple Blog?</title>
+
+
+</head>
+
+<body class="default">
+<div class="wrapper">
+
+<nav class="nav">
+    <a style="border:none;" id="logo" href="index.php"><h1>Simple<span>-</span>Blog</h1></a>
+    <ul class="nav-primary">
+        <li><a href="new_post.php">+ Tambah Post</a></li>
+    </ul>
+</nav>
+
+<article class="art simple post">
+	
+    <header class="art-header">
+        <div class="art-header-inner" style="margin-top: 0px; opacity: 1;">
+            <time class="art-time"><?php echo $row['tanggal'];?></time>
+            <h2 class="art-title"><?php echo $row['judul'];?></h2>
+            <p class="art-subtitle"></p>
+        </div>
+    </header>
+
+    <div class="art-body">
+        <div class="art-body-inner">
+            <hr class="featured-article" />
+            <p><?php echo $row['konten']; ?></p>
+
+            <hr />
+            
+            <h2>Komentar</h2>
+
+            <div id="contact-area">
+                <form name="comment" method="post">
+                    <label for="Nama">Nama:</label>
+                    <input type="text" name="Nama" id="Nama">
+        
+                    <label for="Email">Email:</label>
+                    <input type="text" name="Email" id="Email">
+                    
+                    <label for="Komentar">Komentar:</label><br>
+                    <textarea name="Komentar" rows="20" cols="20" id="Komentar"></textarea>
+					
+					
+                    <input type="button" name="submit" value="Kirim" class="submit-button" onclick="return callajax(<?php echo $var?>)">
+                </form>
+            </div>
+            <ul class="art-list-body" id = "allcom">
+              <div id="com">  
+				<?php
+					$sqli = mysql_query("SELECT * FROM komentar ORDER BY id DESC");
+					while($baris = mysql_fetch_array($sqli)){
+						$id = $baris['id'];
+						$nama = $baris['nama'];
+						$email = $baris['email'];
+						$tanggal = $baris['tanggal'];
+						$isikomen = $baris['isikomen'];
+				?>
+				<li class="art-list-item">
+                    <div class="art-list-item-title-and-time">
+                        <h2 class="art-list-title"><a href="post.php?postid=<?php echo $var;?>"><?php echo $nama; ?></a></h2>
+                        <div class="art-list-time"><?php echo $tanggal; ?></div>
+                    </div>
+                    <p> <?php echo $isikomen; ?></p>
+				</li>
+				<?php } ?>
+			</div>
+            </ul>
+        </div>
+    </div>
+
+</article>
+
+<footer class="footer">
+    <div class="back-to-top"><a href="">Back to top</a></div>
+    <!-- <div class="footer-nav"><p></p></div> -->
+    <div class="psi">&Psi;</div>
+    <aside class="offsite-links">
+        Asisten IF3110 /
+        <a class="rss-link" href="#rss">RSS</a> /
+        <br>
+        <a class="twitter-link" href="http://twitter.com/YoGiiSinaga">Yogi</a> /
+        <a class="twitter-link" href="http://twitter.com/sonnylazuardi">Sonny</a> /
+        <a class="twitter-link" href="http://twitter.com/fathanpranaya">Fathan</a> /
+        <br>
+        <a class="twitter-link" href="#">Renusa</a> /
+        <a class="twitter-link" href="#">Kelvin</a> /
+        <a class="twitter-link" href="#">Yanuar</a> /
+        
+    </aside>
+</footer>
+
+</div>
+
+<script type="text/javascript" src="assets/js/fittext.js"></script>
+<script type="text/javascript" src="assets/js/app.js"></script>
+<script type="text/javascript" src="assets/js/respond.min.js"></script>
+<script type="text/javascript">
+  var ga_ua = '{{! TODO: ADD GOOGLE ANALYTICS UA HERE }}';
+
+  (function(g,h,o,s,t,z){g.GoogleAnalyticsObject=s;g[s]||(g[s]=
+      function(){(g[s].q=g[s].q||[]).push(arguments)});g[s].s=+new Date;
+      t=h.createElement(o);z=h.getElementsByTagName(o)[0];
+      t.src='//www.google-analytics.com/analytics.js';
+      z.parentNode.insertBefore(t,z)}(window,document,'script','ga'));
+      ga('create',ga_ua);ga('send','pageview');
+</script>
+<script>
+  // The variable that makes Ajax possible!
+ var id;
+function callajax(id){
+if (validateemail())
+{
+ var ajaxRequest;
+ try{ 
+   // Opera 8.0+, Firefox, Safari
+   ajaxRequest = new XMLHttpRequest();
+ }catch (e){
+   // Internet Explorer Browsers
+   try{
+      ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+   }catch (e) {
+      try{
+         ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+      }catch (e){
+         // Something went wrong
+         alert("Your browser broke!");
+         return false;
+      }
+   }}
+//alert("tes");	   
+   var jsvar1 = document.forms["comment"]["Nama"].value;
+   var jsvar2 = document.forms["comment"]["Email"].value;
+   var jsvar3 = document.forms["comment"]["Komentar"].value;
+   
+   var parameter;
+   parameter = "Nama="+jsvar1+"&Email="+jsvar2+"&Komentar="+jsvar3+"&postid="+id;
+	//alert(parameter);
+  ajaxRequest.onreadystatechange=function() {
+    if (ajaxRequest.readyState==4 && ajaxRequest.status==200) {
+		 document.getElementById('allcom').innerHTML = ajaxRequest.responseText + document.getElementById('allcom').innerHTML + document.getElementById('com').innerHTML;  	 
+		 
+    }
+  }
+  ajaxRequest.open("POST","comment.php",true);
+  ajaxRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+  ajaxRequest.send(parameter);
+ }
+ }
+ </script>
+ <script type="text/javascript">
+function validateemail(){
+var email = document.getElementById('Email');
+ var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!filter.test(email.value)) {
+    alert('Please provide a valid email address');
+    email.focus;
+    return false;
+ }
+ else
+	return true;
+}
+ </script>
+
+</body>
+</html>
