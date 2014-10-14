0d200835c017112e051b487b8945555707e0c6ce
Andre Susanto <andre@andresusanto.info>
2014-10-14 09:25:57 +0700

    Tambah validasi email

--- post.php
+++ post.php
@@ -69,6 +69,12 @@ function refreshKomentar() {
 }
 
 function submitKomentar(){
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (! re.test(document.getElementById("Email").value)){
+		alert('email anda tidak valid');
+		return false;
+	}
+	
 	var xmlhttp;
     xmlhttp = new XMLHttpRequest();
     
