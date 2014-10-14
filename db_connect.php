37c40b294ccd34ea8d28b038ef42d55ee69d1b56
Farizan Ramadhan <farizanramadhan@gmail.com>
2014-10-14 09:15:36 +0700

    Create db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_SERVER', 'localhost'); // eg, localhost - should not be NULL
+define('DB_USERNAME', 'root');  // MySql user
+define('DB_PASSWORD', ''); // MySql user password
+define('DB_DATABASE', 'tugasweb'); // MySql DB
+?>
