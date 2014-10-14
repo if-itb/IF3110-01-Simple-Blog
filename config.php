53939626f76cdf958e1457d48931d97e07438c6c
ichakid <choirunnisa.fatima@gmail.com>
2014-10-15 00:18:49 +0700

    Menambah timezone

--- config.php
+++ config.php
@@ -11,4 +11,7 @@ define('DBNAME','simple-blog');
 $db = new PDO("mysql:host=".DBHOST.";port=3306;dbname=".DBNAME, DBUSER, DBPASS);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+//set timezone
+date_default_timezone_set('Asia/Bangkok');
+
 ?>
