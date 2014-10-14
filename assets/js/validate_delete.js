1814c6551589d315d9a18704f7a69d7d902a644c
runbow <dragoon.redeyed@gmail.com>
2014-10-14 14:37:36 +0700

    clean up

--- assets/js/validate_delete.js
+++ assets/js/validate_delete.js
@@ -1,12 +1,9 @@
 function validatedelete(teks)
 {
 	var x;
-	var y = teks;
-	var z = "delete_post.php?id=";
-	var res = z.concat(y);
 	if (confirm("Apakah Anda yakin menghapus post ini?")==true)
 		{
-			x=window.location.href=res;
+			x=window.location.href="delete_post.php?id="+teks;
 		}else{
 			x="cancel"; 
 		}
