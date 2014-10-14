56cf9a3282ec483f348d5456931a06c1d8aef07f
imballinst <lucent_rays@yahoo.com>
2014-10-15 03:53:48 +0700

    Revert "first commit"
    This reverts commit be33e729a1ef8bbef244f4b31f27e1309f71ac02.

--- delete-comment.php
+++
@@ -1,7 +0,0 @@
-<?php
-// comment_delete.php
-include 'mysql.php';
-mysql_safe_query('DELETE FROM comments WHERE id=%s LIMIT 1', $_GET['id']);
-mysql_safe_query('UPDATE posts SET num_comments=num_comments-1 WHERE id=%s LIMIT 1', $_GET['post']);
-redirect('post.php?id='.$_GET['post']);
-?>
--- delete_post.php
+++
@@ -1,7 +0,0 @@
-<?php
-// post_delete.php
-include 'mysql.php';
-mysql_safe_query('DELETE FROM posts WHERE id=%s LIMIT 1', $_GET['id']);
-mysql_safe_query('DELETE FROM comments WHERE post_id=%s', $_GET['id']);
-redirect('index.php');
-?>
