9700b6b894a0e54484626e9e7bc328ac3241f375
akhfa <akhmadfakhoni@gmail.com>
2016-02-24 14:55:24 +0700

    create content ok

--- App/Controller/PostController.php
+++ App/Controller/PostController.php
@@ -8,9 +8,10 @@
 
 namespace App\Controller;
 
-use App\Core\ConfigLoader;
 use App\Core\Controller as BaseController;
 use App\Core\View;
+use App\Core\DB\PDOConnection;
+use App\Core\SessionManager;
 
 Class PostController extends BaseController{
     public function index()
@@ -19,5 +20,61 @@ Class PostController extends BaseController{
         $view->inject('navbar', 'navbar');
         $view->inject('content', 'post');
         echo $view->output();
+    }
+    public function getView($id)
+    {
+        $view = new View('layout');
+        $view->inject('navbar', 'navbar');
+        $view->inject('content', 'post_content');
+        echo $view->output();
+    }
+    public function getCreate()
+    {
+        $view = new View('layout');
+        $view->inject('navbar', 'navbar');
+        $view->inject('content', 'post_form');
+        echo $view->output();
+    }
+    public function postCreate()
+    {
+        // Set html purifier
+        require_once ROOT_PATH.'/App/library/HTMLPurifier.auto.php';
+        $purifier = new \HTMLPurifier();
+
+        if (!isset($_POST)) {
+            throw new \RuntimeException("No data posted.", 400);
+        }
+//
+        if (!isset($_POST['judul']) or !isset($_POST['konten'])) {
+            throw new \RuntimeException("Missing title or konten.", 400);
+        }
+
+        $session = SessionManager::getManager();
+        $user = $session->get('user');
+
+        $username = $user['username'];
+
+        // Filter input
+        $judul = strip_tags($_POST['judul']);
+        $konten = $purifier->purify($_POST['konten']);
+
+        $connection = PDOConnection::getInstance();
+        $result = $connection->insert('posts', [
+            'title' => $judul,
+            'content' => $konten,
+            // TODO: change 1 jadi id user berdasarkan username
+            'user_id' => 1,
+        ]);
+
+        if($result)
+        {
+            header('Location: /', true, 200);
+        }
+        else {
+            $error = $connection->getDriver()->errorInfo();
+            var_dump($error);
+
+            throw new \RuntimeException("Cannot create post:", 500);
+        }
     }
 }
--- resources/views/post_content.tpl
+++ resources/views/post_content.tpl
@@ -1,21 +1,13 @@
 <div class="container">
     <div class="row">
-        <h2 class="center-align">Daftar Post</h2>
+        <h2 class="center-align">Login</h2>
         <div class="row">
             <div class="col s12">
                 <div class="card">
-                    <div class="card-content">
+                    <div class="card-image">
                         <span class="card-title">Card Title</span>
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                            I am convenient because I require little markup to use effectively.</p>
                     </div>
-                    <div class="card-action">
-                        <a href="#">Read more</a>
-                    </div>
-                </div>
-                <div class="card">
                     <div class="card-content">
-                        <span class="card-title">Card Title</span>
                         <p>I am a very simple card. I am good at containing small bits of information.
                             I am convenient because I require little markup to use effectively.</p>
                     </div>
@@ -25,5 +17,22 @@
                 </div>
             </div>
         </div>
+
+        <div class="divider"></div>
+        <div class="section">
+            <h5>Komentar 1</h5>
+            <p>Stuff</p>
+        </div>
+        <div class="divider"></div>
+        <div class="section">
+            <h5>Komentar 2</h5>
+            <p>Stuff</p>
+        </div>
+        <div class="divider"></div>
+        <div class="section">
+            <h5>Komentar 3</h5>
+            <p>Stuff</p>
+        </div>
+
     </div>
 </div>
