94221e765185aa4ffea45565b0763a7172d81749
akhfa <akhmadfakhoni@gmail.com>
2016-02-25 15:33:27 +0700

    update schema

--- config/schema.sql
+++ config/schema.sql
@@ -1,38 +1,48 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.1.6-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             9.3.0.4984
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 25, 2016 at 03:32 PM
+-- Server version: 10.1.11-MariaDB-1~trusty-log
+-- PHP Version: 5.5.9-1ubuntu4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
--- Dumping database structure for simple_blog
-CREATE DATABASE IF NOT EXISTS `simple_blog` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `simple_blog`;
+--
+-- Database: `kpi_blog`
+--
 
+-- --------------------------------------------------------
 
--- Dumping structure for table simple_blog.comments
+--
+-- Table structure for table `comments`
+--
+
 CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(256) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `title` varchar(25) NOT NULL,
   `content` text NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
+  `post_id` int(4) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `metadata` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
--- Dumping data for table simple_blog.comments: ~0 rows (approximately)
-/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `files`
+--
 
--- Dumping structure for table simple_blog.files
 CREATE TABLE IF NOT EXISTS `files` (
   `id` char(36) NOT NULL,
   `path` text NOT NULL,
@@ -45,12 +55,12 @@ CREATE TABLE IF NOT EXISTS `files` (
   KEY `mime` (`mime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table simple_blog.files: ~0 rows (approximately)
-/*!40000 ALTER TABLE `files` DISABLE KEYS */;
-/*!40000 ALTER TABLE `files` ENABLE KEYS */;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `posts`
+--
 
--- Dumping structure for table simple_blog.posts
 CREATE TABLE IF NOT EXISTS `posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` text NOT NULL,
@@ -58,18 +68,17 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `user_id` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `highlight_image_id` char(36) NOT NULL,
+  `highlight_image_id` char(36) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `highlight_image_files_id` (`highlight_image_id`),
-  CONSTRAINT `highlight_image_files_id` FOREIGN KEY (`highlight_image_id`) REFERENCES `files` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `highlight_image_files_id` (`highlight_image_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
--- Dumping data for table simple_blog.posts: ~0 rows (approximately)
-/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `sessions`
+--
 
--- Dumping structure for table simple_blog.sessions
 CREATE TABLE IF NOT EXISTS `sessions` (
   `id` char(128) NOT NULL,
   `payload` text NOT NULL,
@@ -77,12 +86,12 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table simple_blog.sessions: ~6 rows (approximately)
-/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `users`
+--
 
--- Dumping structure for table simple_blog.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(32) NOT NULL,
@@ -92,13 +101,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
--- Dumping data for table simple_blog.users: ~1 rows (approximately)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `username`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-	(1, 'admin', '$2a$10$wvZ0n//S0EEV3KOL4cFvhuU6u3BX3p6Rb0IdtX.TmQgOfcyQn8R0W', NULL, '2016-02-24 11:29:54', '2016-02-24 11:29:54');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `highlight_image_files_id` FOREIGN KEY (`highlight_image_id`) REFERENCES `files` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
